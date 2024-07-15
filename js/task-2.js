8c17689a539a578c4ea6a0d6d3cf9318e45666be
Birsen <birsen-ozer@hotmail.com>
2024-07-15 23:43:12 +0300

    update

--- js/task-2.js
+++ js/task-2.js
@@ -1,8 +1,8 @@
-function formatMessage(message, Length) {
-  if (message.Length <= Length) {
+function formatMessage(message, maxLength) {
+  if (message.length <= maxLength) {
     return message;
   } else {
-    return message.slice(0, Length) + '...';
+    return message.slice(0, maxLength) + '...';
   }
 }
 
